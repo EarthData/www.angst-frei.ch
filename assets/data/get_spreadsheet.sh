c98c037bc34da53ad039afa8dbc78aad58727292
Joerg Roth <joerg.roth@geo.uzh.ch>
2021-01-05 17:10:05 +0100

    update 2021010506

---
+++ assets/data/get_spreadsheet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#wget https://docs.google.com/spreadsheets/d/e/2PACX-1vSz2DhNmbULD0ERdq9IAf7XmfgeUVVw_v9lMzOr0YZOe1kTyIc2rYntFCxCUOPLyhkuT0DcLpD12VKy/pub?gid=162516140&single=true&output=csv -O lokal_schweiz.csv
+
+wget https://docs.google.com/spreadsheets/d/e/2PACX-1vSz2DhNmbULD0ERdq9IAf7XmfgeUVVw_v9lMzOr0YZOe1kTyIc2rYntFCxCUOPLyhkuT0DcLpD12VKy/pub?output=csv -O lokal_schweiz.csv
