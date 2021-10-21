85601a3774618b6be680a2967871b509ca6543e0
Joerg Roth <joerg.roth@geo.uzh.ch>
2021-10-21 17:22:38 +0200

    update 2021102101

---
+++ _posts/2019-09-21-event_cabin-talk.md
@@ -0,0 +1,15 @@
+---
+date:        2019-09-21
+title:       Ereignis
+subtitle:    'The next pandemic is just around the corner!'
+description: 'If there‘s an award for the best "I told you so" moment, then this women deserves to win it.'
+country:     [US]
+timeline:    P(l)andemie
+group:       Ereignis
+categories:  [P(l)andemie]
+tags:        []
+youtubeId:   "wV9Rl6d2Mys"
+---
+If there's an award for the best "I told you so" moment, then this women deserves to win it.
+
+{% include youtubePlayer.html id=page.youtubeId %}
--- assets/js/bar-chart-cap-v7.js
+++ assets/js/bar-chart-cap-v7.js
@@ -3,180 +3,286 @@ var margin = {top: 30, right: 70, bottom: 40, left: 40},
   width = width - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
+var y_orig = {};
+var active_link = {};
+var legendClassArray = {};
+var nodes = {};
+var idx = {};
+
 var parseTime  = d3.timeParse("%Y-%m-%d");
 
-const graph = async (region, type, ydomain) => {
+// List of groups (here I have one group per column)
+var allGroup = ["ZH", "AG", "AI", "AR", "BE", "BL", "BS", "FR", "GE", "GL", "GR", "JU", "LU", "NE", "NW", "OW", "SG", "SH", "SO", "SZ", "TG", "TI", "UR", "VD", "VS", "ZG"]
+var divs = ["ch-icu", "ch-nonicu", "region-icu", "region-nonicu"]
 
-  var title = region + " Spital-Betten " + type.toUpperCase();
+divs.forEach(function(d) {
+  d3.select("#" + d).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("class", "svg-" + d)
+});
 
-  var group = region.toLowerCase() + "-" + type;
+// add the options to the button
+d3.select("#selectButton")
+  .selectAll('myOptions')
+  .data(allGroup)
+  .enter()
+  .append('option')
+  .text((d) => d) // text showed in the menu
+  .attr('value', (d) => d) // corresponding value returned by the button
+
+const graph = async (region, type) => {
+
+  if (type == "covid") {
+    var title = region + " Hospitalisationen nach Alter (Covid) ";
+  } else {
+    var title = region + " Hospitalisationen nach Alter (Covid-geimpft) ";
+  }
+
+  var group = region == "CH" ? region.toLowerCase() + "-" + type : "region-" + type;
+
+  active_link[group] = "0"; //to control legend selections and hover
+  legendClassArray[group] = []; //store legend classes to select bars in plotSingle()
+  y_orig[group]; //to store original y-posn
 
   var graph = await d3.csv("data_hosp_processed/hosp_" + region + "-" + type + ".csv")
     .then(function(data) {
 
+    var keys = data.columns.slice(1)
+
     data.forEach(function(d) {
+      for (i = 0, t = 0; i < keys.length; ++i) t += d[keys[i]] = +d[keys[i]];
+      d.total = t;
+      //y0 = 0;
       d.day = parseTime(d.day);
+      //d.types = keys.map(function(name) { return {myday: d.day, name: name, y0: y0, y1: y0 += +d[name]}; });
     });
 
-    var keys = data.columns.slice(1)
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, (d) => d.day))
+      .range([0, width - 24]);
+
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, (d) => d.total)]).nice()
+      .rangeRound([height, 0]);
 
     var color = d3.scaleOrdinal()
       .domain(keys)
       .range(["#660099", "#B319FF", "#99FF66"]);
 
-    //stack the data?
     var stackedData = d3.stack()
       .keys(keys)
       (data)
 
-    var svg = d3.select("#" + group)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var xAxis = d3.axisBottom(x);
+    var yAxis = d3.axisLeft(y).tickFormat(d3.format(".2s"));
 
-    svg.append("text")
-      .attr("x", (width / 2))
-      .attr("y", 0 - (margin.top / 2))
-      .attr("text-anchor", "middle")
-      .style("font-size", "16px")
-      .style("text-decoration", "underline")
-      .text(title);
+    d3.selectAll(".g-" + group)
+      .remove()
 
-    // Add X axis
-    var x = d3.scaleTime()
-      .domain(d3.extent(data, function(d) { return d.day; }))
-      .range([ 0, width ]);
+    var svg = d3.select('.svg-' + group)
+      .append('g')
+      .attr('class', 'g-' + group)
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+   
+    svg.append('g')
+      .attr('class', 'x axis')
+      .call(xAxis)
+      .attr('transform', 'translate(0,' + height + ')')
 
-    var xAxis = svg.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x).ticks(10))
+    svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis)
 
-    // Add Y axis
-    const y = d3.scaleLinear()
-      .domain([0, ydomain])
-      .range([ height, 0 ]);
+    svg.append('text')
+      .attr('x', (width / 2))
+      .attr('y', 0 - (margin.top / 2))
+      .attr('text-anchor', 'middle')
+      .attr('class', 'title-' + group)
+      .style('font-size', '16px')
+      .style('text-decoration', 'underline')
+      .text(title)
 
-    svg.append("g")
-      .call(d3.axisLeft(y).ticks(5))
-
-    //////////
-    // BRUSHING AND CHART //
-    //////////
-
-    // Add a clipPath: everything out of this area won't be drawn.
-    const clip = svg.append("defs").append("svg:clipPath")
-      .attr("id", "clip")
-      .append("svg:rect")
-      .attr("width", width )
-      .attr("height", height )
-      .attr("x", 0)
-      .attr("y", 0);
-
-    // Add brushing
-    const brush = d3.brushX()                 // Add the brush feature using the d3.brush function
-      .extent( [ [0,0], [width,height] ] ) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-      .on("end", updateChart) // Each time the brush selection changes, trigger the 'updateChart' function
-
-    // Create the scatter variable: where both the circles and the brush take place
-    const areaChart = svg.append('g')
-      .attr("clip-path", "url(#clip)")
-
-    // Area generator
-    const area = d3.area()
-      .x(function(d) { return x(d.data.day); })
-      .y0(function(d) { return y(d[0]); })
-      .y1(function(d) { return y(d[1]); })
-
-    // Show the areas
-    areaChart
-      .selectAll("mylayers")
+    var state = svg.selectAll('.state')
       .data(stackedData)
-      .join("path")
-      .attr("class", function(d) { return group + "-area " + group + "-area-" + d.key })
-      .style("fill", function(d) { return color(d.key); })
-      .attr("d", area)
+      .join('path')
+      .transition()
+      .ease(d3.easeLinear)
+      .duration(800)
+      .delay(function (d, i) {
+          return i * 50;
+      })
+      .attr("d", d3.area()
+        .x((d) => x(d.data.day))
+        .y0((d) => y(d[0]))
+        .y1((d) => y(d[1]))
+      )
+      .attr('fill', (d) => color(d.key))
+      .attr('class', (d) => 'class-' + group + '-' + d.key)
+      .style('opacity', 1);
 
-    // Add the brushing
-    areaChart
-      .append("g")
-      .attr("class", "brush")
-      .call(brush);
+/*
+    state.selectAll("rect")
+     .on("mouseover", function(d,i){
+        var delta = i.y1 - i.y0;
+        var xPos = parseFloat(d3.select(this).attr("x"));
+        var yPos = parseFloat(d3.select(this).attr("y"));
+        var height = parseFloat(d3.select(this).attr("height"))
+        d3.select(this).attr("stroke","blue").attr("stroke-width",0.8);
+        svg.append("text")
+          .attr("x", xPos)
+          .attr("y", yPos + height/2)
+          .attr("class", group + "-tooltip")
+          .text(i.name + ": " + delta);
+      })
 
-    let idleTimeout
-    function idled() { idleTimeout = null; }
+      .on("mouseout",function(){
+        svg.select("." + group + "-tooltip").remove();
+        d3.select(this).attr("stroke","pink").attr("stroke-width",0.2);
+      })
+*/
 
-    // A function that update the chart for given boundaries
-    function updateChart(event,d) {
+      var legend = svg.selectAll(".legend")
+        .data(color.domain().slice().reverse())
+        .enter().append("g")
+        .attr("id", function (d) {
+          legendClassArray[group].push(group + "-" + d.replace(/\s/g, '')); //remove spaces
+          return "legend-" + group;
+        })
+        .attr("transform", function(d, i) { return "translate(50," + i * 20 + ")"; });
 
-      extent = event.selection
+      //reverse order to match order in which bars are stacked
+      legendClassArray[group] = legendClassArray[group].reverse();
 
-      // If no selection, back to initial coordinate. Otherwise, update X axis domain
-      if(!extent){
-        if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
-        x.domain(d3.extent(data, function(d) { return d.day; }))
-      } else {
-        x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
-        areaChart.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
+      legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color)
+        .attr("id", function (d, i) {
+          return "id-" + group + "-" + d.replace(/\s/g, '');
+        })
+
+      .on("mouseover",function() {
+        if (active_link[group] === "0") d3.select(this).style("cursor", "pointer");
+        else {
+          if (active_link[group].split("class").pop() === this.id.replace('id-','')) {
+            d3.select(this).style("cursor", "pointer");
+          } else d3.select(this).style("cursor", "auto");
+        }
+      })
+
+      .on("click",function(d, i) {
+        let id = this.id.matchAll(/^id-([a-z]{2,6})-([a-z]+)-(.*)/g);
+        id = Array.from(id);
+        group = id[0][1] + "-" + id[0][2]
+        if (active_link[group] === "0") { //nothing selected, turn on this selection
+          d3.select(this)
+            .style("stroke", "black")
+            .style("stroke-width", 2);
+            active_link[group] = this.id.replace('id-','');
+            plotSingle(this);
+            //gray out the others
+            for (j = 0; j < legendClassArray[group].length; j++) {
+              if (legendClassArray[group][j] != active_link[group]) {
+                d3.select("#id-" + group + "-" + legendClassArray[group][j])
+                  .style("opacity", 0.5);
+              }
+            }
+        } else { //deactivate
+          if (active_link[group] === this.id.replace('id-','')) {//active square selected; turn it OFF
+            d3.select(this)
+              .style("stroke", "none");
+            active_link[group] = "0"; //reset
+            //restore remaining boxes to normal opacity
+            for (j = 0; j < legendClassArray[group].length; j++) {
+              d3.select("#id-" + group + "-" + legendClassArray[group][j])
+                .style("opacity", 1);
+            }
+            //restore plot to original
+            restorePlot(d, group);
+          }
+        } //end active_link check
+      });
+  
+      legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
+  
+      function restorePlot(d, group) {
+/*
+        state.nodes().forEach(function(d, i) {
+          nodes[group] = d.childNodes;
+          //restore shifted bars to original posn
+          d3.select(nodes[group][idx[group]])
+            .transition()
+            .duration(1000)
+            .attr("y", y_orig[group][i]);
+        })
+*/
+        //restore opacity of erased bars
+        for (i = 0; i < legendClassArray[group].length; i++) {
+          if (legendClassArray[group][i] != class_keep) {
+            d3.selectAll(".class-" + legendClassArray[group][i])
+              .transition()
+              .duration(1000)
+              .delay(750)
+              .style("opacity", 1);
+          }
+        }
       }
+  
+      function plotSingle(d) {
+        class_keep = d.id.replace('id-','');
+        group = d.parentElement.id.replace('legend-','');
+        idx[group] = legendClassArray[group].indexOf(class_keep);
+        //erase all but selected bars by setting opacity to 0
+        for (i = 0; i < legendClassArray[group].length; i++) {
+          if (legendClassArray[group][i] != class_keep) {
+            d3.selectAll(".class-" + legendClassArray[group][i])
+              .transition()
+              .duration(1000)
+              .style("opacity", 0);
+          }
+        }
 
-      // Update axis and area position
-      xAxis.transition().duration(1000).call(d3.axisBottom(x).ticks(5))
-      areaChart
-        .selectAll("path")
-        .transition().duration(1000)
-        .attr("d", area)
+/*
+        //lower the bars to start on x-axis
+        y_orig[group] = [];
+
+        state.nodes().forEach(function(d, i) {
+          nodes[group] = d.childNodes;
+          //get height and y posn of base bar and selected bar
+          console.log(d3.select(nodes[group][idx[group]]));
+          h_keep = d3.select(nodes[group][idx[group]]).attr("y1");
+          y_keep = d3.select(nodes[group][idx[group]]).attr("y0");
+
+          console.log(h_keep);
+          console.log(y_keep);
+            
+          //store y_base in array to restore plot
+          y_orig[group].push(y_keep);
+
+          h_base = d3.select(nodes[group][0]).attr("y1");
+          y_base = d3.select(nodes[group][0]).attr("y0");
+
+          h_shift = h_keep - h_base;
+          y_new = y_base - h_shift;
+
+          //reposition selected bars
+          d3.select(nodes[group][idx[group]])
+            .transition()
+            .ease(d3.easeBounce)
+            .duration(1000)
+            .delay(750)
+            .attr("y1", h_shift)
+            .attr("y0", y_new);
+        })
+*/
     }
-
-    //////////
-    // HIGHLIGHT GROUP //
-    //////////
-
-    // What to do when one group is hovered
-    const highlight = function(event,d){
-      // reduce opacity of all groups
-      d3.selectAll("." + group + "-area")
-        .style("opacity", .1)
-      // expect the one that is hovered
-      d3.select("." + group + "-area-" + d).style("opacity", 1)
-    }
-
-    // And when it is not hovered anymore
-    const noHighlight = function(event,d){
-      d3.selectAll("." + group + "-area").style("opacity", 1)
-    }
-
-    //////////
-    // LEGEND //
-    //////////
-
-    // Add one dot in the legend for each name.
-    const size = 15
-    svg.selectAll("myrect")
-      .data(keys)
-      .join("rect")
-      .attr("x", width - 80)
-      .attr("y", function(d,i){ return 0 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
-      .attr("width", size)
-      .attr("height", size)
-      .style("fill", function(d){ return color(d)})
-      .on("mouseover", highlight)
-      .on("mouseleave", noHighlight)
-
-    // Add one dot in the legend for each name.
-    svg.selectAll("mylabels")
-      .data(keys)
-      .join("text")
-      .attr("x", width - 80 + size*1.2)
-      .attr("y", function(d,i){ return 0 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
-      .style("fill", "black")
-      .text(function(d){ return d})
-      .attr("text-anchor", "left")
-      .style("alignment-baseline", "middle")
-      .on("mouseover", highlight)
-      .on("mouseleave", noHighlight)
-    
   })
   .catch(function(error){
     throw error;
@@ -185,23 +291,21 @@ const graph = async (region, type, ydomain) => {
 }
 
 const build = async () => {
-  await graph("CH", "nonicu", 25000);
-  await graph("CH", "icu", 1600);
-  await graph("ZH", "nonicu", 5000);
-  await graph("ZH", "icu", 500);
-  await graph("AG", "nonicu", 1500);
-  await graph("AG", "icu", 100);
-  await graph("BE", "nonicu", 3500);
-  await graph("BE", "icu", 150);
-  await graph("GE", "nonicu", 3200);
-  await graph("GE", "icu", 140);
-  await graph("SG", "nonicu", 1500);
-  await graph("SG", "icu", 100);
-  await graph("SO", "nonicu", 600);
-  await graph("SO", "icu", 40);
-  await graph("SZ", "nonicu", 400);
-  await graph("SZ", "icu", 20);
+  await graph("CH", "icu");
+  await graph("CH", "nonicu");
+  await graph("ZH", "icu");
+  await graph("ZH", "nonicu");
 }
 
 build();
 
+// When the button is changed, run the updateChart function
+d3.select("#selectButton").on("change", function(d) {
+  var selectedOption = d3.select(this).property("value")
+  changeDrop(selectedOption)
+})
+
+const changeDrop = async(region) => {
+  await graph(region, "icu");
+  await graph(region, "nonicu");
+}
