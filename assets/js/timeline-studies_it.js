5f3e9f89c0badbb6b12ecae416367ed5156c1585
Joerg Roth <joerg.roth@geo.uzh.ch>
2021-11-17 10:42:30 +0100

    add studies-timeline

---
+++ assets/js/timeline-studies_it.js
@@ -0,0 +1,11 @@
+json_template = '/json/timeline-studies_it.json'
+var options = {
+  language: "it",
+  width: "100%",
+  height: "100%",
+  initial_zoom: 3,
+  slide_padding_lr: 100,
+  hash_bookmark: true,
+  start_at_end: false,
+}
+window.timeline = new TL.Timeline('timeline-embed', json_template, options);
